387347ac8a60d7fa593e8f8aff400fb6d9401e9f
Jacques du Rand <jacquesdr@gmail.com>
2015-01-06 23:17:10 +0200

    renaming

--- rest/rpweb/models/models.go
+++ rest/rpweb/models/models.go
@@ -6,9 +6,28 @@ import (
 	//"github.com/go-sql-driver/mysql"
 	_ "github.com/rawoke083/rpweb/storage"
 
-	//"time"
+	"time"
 )
 
 /****************************
  * DB/Domain Models
  ***************************/
+type Rilt struct {
+	ID           int64
+	Concept_id       int64
+	Usr_Id		int64
+	
+	Text       string
+	Text_Md  string
+	Type int
+	Date_created time.Time
+}
+
+type Concept struct {
+	ID           int64
+	Title        string
+	Usr_Id       int64 `db:"usr_id"`
+	Description  string
+	Date_updated time.Time
+	Date_created time.Time
+}
